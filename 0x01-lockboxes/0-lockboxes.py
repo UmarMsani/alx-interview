9ace65fb99727b543c3e45abc3451605dc27cecc
UmarMsani <umuhammad220@gmail.com>
2024-03-12 03:19:53 -0700

    Implement canUnlockAll function for checking if all boxes can be opened.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
+"""Lockboxes Puzzle Solution"""
+
 
 def canUnlockAll(boxes):
-    if not boxes or not isinstance(boxes, list):
+    """Determines whether all the boxes can be unlocked"""
+    if not isinstance(boxes, list) or len(boxes) == 0:
         return False
 
-    n = len(boxes)
-    if n == 0:
-        return False
+    for key_to_check in range(1, len(boxes) - 1):
+        unlocked = False
+        for box_index in range(len(boxes)):
+            unlocked = key_to_check in boxes[box_index] and key_to_check != box_index
+            if unlocked:
+                break
+        if not unlocked:
+            return unlocked
 
-    # Set to keep track of opened boxes
-    opened_boxes = set()
-
-    # Initialize with the first box (index 0)
-    opened_boxes.add(0)
-
-    # Queue to keep track of keys to explore
-    keys_to_explore = boxes[0]
-
-    while keys_to_explore:
-        key = keys_to_explore.pop(0)
-
-        if key < n and key not in opened_boxes:
-            # Open the box and add its keys to the queue
-            opened_boxes.add(key)
-            keys_to_explore.extend(boxes[key])
-
-    # Check if all boxes have been opened
-    return len(opened_boxes) == n
+    return True
