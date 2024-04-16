fd15451a91cb795a87f9a76afff407da80107b83
UmarMsani <umuhammad220@gmail.com>
2024-04-16 16:03:00 -0700

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
+  request(`${API_URL}/films/${process.argv[2]}/`, (error, _, body) => {
+    if (error) {
+      console.log(error);
     }
-
     const charactersURL = JSON.parse(body).characters;
-
     const charactersName = charactersURL.map(
       url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
+        request(url, (promiseError, __, charactersReqBody) => {
+          if (promiseError) {
+            reject(promiseError);
           }
-
           resolve(JSON.parse(charactersReqBody).name);
         });
       }));
 
     Promise.all(charactersName)
       .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+      .catch(allError => console.log(allError));
   });
-
 }
