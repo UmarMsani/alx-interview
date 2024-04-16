3c7d954779966ea4412087a21adbd3c15eaefcec
UmarMsani <umuhammad220@gmail.com>
2024-04-16 15:36:24 -0700

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,23 +3,18 @@
 const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
-// Check if a movie ID is provided as a command-line argument
 if (process.argv.length > 2) {
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
       console.log(err);
     }
-	
-    // Extract URLs of characters appearing in the movie
     const charactersURL = JSON.parse(body).characters;
-     // Map character URLs to promises fetching character names
     const charactersName = charactersURL.map(
       url => new Promise((resolve, reject) => {
         request(url, (promiseErr, __, charactersReqBody) => {
           if (promiseErr) {
             reject(promiseErr);
           }
-	   // Resolve promise with character name
           resolve(JSON.parse(charactersReqBody).name);
         });
       }));
