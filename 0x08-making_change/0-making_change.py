edd90c2817cd579dd1a9ff6d94b47b27ef67fa1e
UmarMsani <umuhammad220@gmail.com>
2024-05-02 06:21:29 -0700

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,17 +11,15 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
+    coins.sort(reverse=True)
+    coins_needed = 0
+    for coin in coins:
+        if total / coin > 0:
+            coins_needed = coins_needed + (total // coin)
+            total = total % coin
+        if not total:
+            break
 
-    dp = [float('inf')] * (total + 1)
-
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    if dp[total] == float('inf'):
+    if total != 0 or coins_needed == 0:
         return -1
-    else:
-        return dp[total]
+    return coins_needed
